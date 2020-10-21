34d23cacf85ad6b0b1b32cafe624dea69c081a8e
Lucca Beurmann <luccab.souza@gmail.com>
2020-10-21 15:34:22 -0300

    Commit inicial

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res)=>{
+    return res.json({msg: "hello world"})
+})
+
+
+app.listen(port, ()=>(console.log(`Servidor rodando na porta ${port}`)))
